b7bb573f66e5397ba03cf335aa57135c6253773e
Jared Scarr <jaredscarr@pm.me>
2022-09-14 14:43:07 -0700

    Refactor of webpack config files into common, prod, and dev. Addition of readme. Modified .gitignore and package.json.

---
+++ README.md
@@ -0,0 +1,17 @@
+# Swiss Tournament Front End
+
+This is the front end code meant to pair with the swiss-tournament API.
+
+## Setup
+This assumes the swiss-tournament repo setup is already complete and has local server started.
+
+1 .Clone the repo
+2. Create .env file with API_BASE_URL set to the host of the fastAPI app you should already have setup
+3. run npm install.
+4. npm run build
+5. npm start
+
+### Note
+
+This was a project to remind myself of vanilla Javascript. Honestly, I would use a framework
+next time :-)
---
+++ webpack.common.js
@@ -0,0 +1,42 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const webpack = require('webpack');
+
+module.exports = {
+  entry: {
+    app: './src/index.js',
+  },
+  module: {
+    rules: [
+      { 
+        test: /\.js$/, 
+        exclude: /node_modules/, 
+        use: { 
+          loader: "babel-loader", 
+          options: { 
+            presets: [ "@babel/preset-env", ] 
+          } 
+        } 
+      }, 
+      { 
+          test: /\.css$/, 
+          use: [ "style-loader", "css-loader" ],
+      }, 
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Swiss Tournament',
+      template: 'src/index.html'
+    }),
+    new webpack.ProvidePlugin({
+      process: 'process/browser',
+    }),
+    new webpack.EnvironmentPlugin(['API_BASE_URL']),
+  ],
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
+  },
+};
